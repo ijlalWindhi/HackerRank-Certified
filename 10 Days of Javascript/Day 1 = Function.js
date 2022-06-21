175da59f77648929033ed0d76c49120894a4bce8
Ijlal Windhi <ijlalwindhi15@gmail.com>
2022-06-22 02:53:13 +0700

    Day 1 Arithmetic and Function

---
+++ 10 Days of Javascript/Day 1 = Function.js
@@ -0,0 +1,45 @@
+// Day 1 : Arithmatic
+"use strict";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", (_) => {
+  inputString = inputString
+    .trim()
+    .split("\n")
+    .map((string) => {
+      return string.trim();
+    });
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+/*
+ * Create the function factorial here
+ */
+
+function factorial(n) {
+  var result = n;
+  while (n > 1) {
+    n--;
+    result *= n;
+  }
+  return result;
+}
+
+function main() {
+  const n = +readLine();
+
+  console.log(factorial(n));
+}
